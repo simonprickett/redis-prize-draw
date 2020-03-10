1643847e7aef5a3f44cca0f415560304b32845a3
Simon Prickett <simon@crudworks.org>
2020-03-09 23:29:47 -0700

    Work in progress.

--- app.py
+++ app.py
@@ -50,23 +50,24 @@ def get_winners():
     winners = redis.hgetall(get_key_name('winners'))
     winner_list = []
 
-    for prize in winners:
-        winner_id = winners[prize]
-        winner_profile = get_github_profile(winner_id)
-        
-        if (winner_profile['name']):
-            winner_name = winner_profile['name']
-        else:
-            winner_name = winner_profile['login']
+    if (winners):
+        for prize in winners:
+            winner_id = winners[prize]
+            winner_profile = get_github_profile(winner_id)
+            
+            if (winner_profile['name']):
+                winner_name = winner_profile['name']
+            else:
+                winner_name = winner_profile['login']
 
-        winner = {}
-        winner['name'] = winner_name
-        winner['prize'] = prize
-        winner['image'] = winner_profile['avatar_url']
-        winner_list.append(winner)
+            winner = {}
+            winner['name'] = winner_name
+            winner['prize'] = prize
+            winner['image'] = winner_profile['avatar_url']
+            winner_list.append(winner)
 
-    # Cache response to save doing this work ove and over.
-    redis.set(get_key_name('winners_json'), json.dumps(winner_list))
+        # Cache response to save doing this work over and over.
+        redis.set(get_key_name('winners_json'), json.dumps(winner_list))
 
     return winner_list
 
@@ -101,8 +102,61 @@ def enter_prize_draw(github_id):
     # Github ID was successfully entered, return the profile.
     return jsonify(profile)
 
-@app.route('/drawprizes') 
+@app.route('/setupdraw')
+def start_new_draw(prizes, duration):
+    # TODO this is a POST request.
+    # TODO CHECK FOR VALID ADMIN SESSION!
+
+    # Delete any previous draw data.  Uses delete not unlink as 
+    # we are about to SADD members to 'prizes' again so want to 
+    # make sure the old 'prizes' was definitely deleted before 
+    # adding the new set of prizes to it...
+    redis.delete(get_key_name('entrants'), get_key_name('winners'), get_key_name('winners_json'), get_key_name('prizes'))
+
+    # TODO START A PIPELINE
+    # TODO write prizes to a set
+    # TODO set is_open with expiry
+    # TODO SUBMIT THE PIPELINE
+    return 'TODO'
+
+@app.route('/drawprizes')
 def draw_prizes():
+    # TODO CHECK FOR VALID ADMIN SESSION!
+
+    # Close the prize draw if it is still open.
+    redis.unlink(get_key_name('is_open'))
+
+    prize_winners = {}
+
+    while True:
+        prize = redis.spop(get_key_name('prizes'), 1)
+
+        if (len(prize) == 0):
+            # We have run out of prizes.
+            break
+    
+        prize = prize[0]
+
+        winner = redis.spop(get_key_name('entrants'), 1)
+
+        if (len(winner) == 0):
+            # We have run out of entrants.
+            break
+
+        winner = winner[0]
+
+        prize_winners[prize] = winner
+
+    # Store winners.
+    redis.hmset(get_key_name('winners'), prize_winners)
+
+    # Delete any remaining (non-winning) entrants.
+    redis.unlink(get_key_name('entrants'))
+
+    return 'Done!'
+
+@app.route('/admin') 
+def admin_page():
     # This needs a get and a post, to cope with
     # rendering a login form and authenticating then
     # running the prize draw...
