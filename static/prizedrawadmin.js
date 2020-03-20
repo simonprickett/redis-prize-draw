6a1cbf6eb07fa814ef5b3ba539123abcaed7c783
Simon Prickett <simon@crudworks.org>
2020-03-19 19:18:30 -0700

    Fixed spacing issue with adding prizes.

--- static/prizedrawadmin.js
+++ static/prizedrawadmin.js
@@ -22,38 +22,38 @@ window.onload = function () {
 
       // Add another prize field.
       const newPrizeTemplate = `
-        <div class="field is-horizontal">
-          <div class="field-label is-normal">
-            <label class="label">Prize</label>
+        <div class="field-label is-normal">
+          <label class="label">Prize</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control has-icons-left">
+            <input id="prize-${nextNumber}-description" class="input prize-description" type="text" placeholder="Prize description...">
+            <span class="icon is-small is-left">
+              <i class="fas fa-gift"></i>
+            </span>
+            </p>
           </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control has-icons-left">
-              <input id="prize-${nextNumber}-description" class="input prize-description" type="text" placeholder="Prize description...">
-              <span class="icon is-small is-left">
-                <i class="fas fa-gift"></i>
-              </span>
-              </p>
-            </div>
-            <div class="field">
-              <div class="control">
-                <button id="prize-${nextNumber}-btn-add" class="button is-primary">
-                  <span class="icon">
-                    <i class="fas fa-plus-circle"></i>
-                  </span>
-                </button>
-                <button id="prize-${nextNumber}-btn-remove" class="is-hidden button is-danger">
-                  <span class="icon">
-                    <i class="fas fa-minus-circle"></i>
-                  </span>
-                </button>
-              </div>  
-            </div>
+          <div class="field">
+            <div class="control">
+              <button id="prize-${nextNumber}-btn-add" class="button is-primary">
+                <span class="icon">
+                  <i class="fas fa-plus-circle"></i>
+                </span>
+              </button>
+              <button id="prize-${nextNumber}-btn-remove" class="is-hidden button is-danger">
+                <span class="icon">
+                  <i class="fas fa-minus-circle"></i>
+                </span>
+              </button>
+            </div>  
           </div>
         </div>`;
 
         const prizes = document.getElementById('prizes');
         const newPrize = document.createElement('div');
+
+        newPrize.className = 'field is-horizontal';
         newPrize.innerHTML = newPrizeTemplate;
 
         prizes.appendChild(newPrize);
